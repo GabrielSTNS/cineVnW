061f2d66aa01bcae96101be386fb07ea2fda44f9
GabrielSTNS <gabriel2000.santana@gmail.com>
2026-02-11 21:10:37 -0300

    :tada: Commit inicial.

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./globalStyle.scss";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const App = () => {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    const buscarDados = async () => {
+      const dados = await axios.get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      );
+
+      try {
+        setFilmes(dados.data.results);
+      } catch (erro) {
+        alert(`Houve um erro ${erro}`);
+      }
+    };
+    buscarDados();
+  }, []);
+  return (
+    <>
+      <header>
+        <h1>
+          Cine<span>VnW</span>
+        </h1>
+        <div className="div">
+          <input type="text" placeholder="Buscar filmes..." />
+          <FaSearch />
+        </div>
+      </header>
+      <main>
+        <section className="boxCard">
+          {filmes.map((item, id) => (
+            <article className="card" key={id}>
+              <div className="card_image">
+                <span>New</span>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                  alt={`Imagem do filme ${item.title}`}
+                />
+                <div className="play">▶</div>
+              </div>
+              <h2>{item.title}</h2>
+              <h3>{Math.ceil(item.vote_average)}</h3>
+              <div>⭐⭐⭐⭐☆</div>
+            </article>
+          ))}
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default App;
